f578f92a6152fbb9b300cc8f8484e3ccd7c0f762
Danil <danilom31@mail.ru>
2025-05-12 10:35:57 +0300

    edits

--- program.js
+++ program.js
@@ -1,27 +1,24 @@
 
 
 function question(idElement){
-    
-    //(document.getElementsByClassName('button')).hidden = true;
+
     let item = document.getElementById(idElement);
     if (item != null){
         f2(item.parentElement);
-        
     }
- 
     (document.getElementsByClassName('button')[0]).remove();
-    console.log(document.children[0].innerHTML);
+
 }
 function f2(current){
     
     if (current != null && current != document.body.parentElement ){
-        
+
         let newElement = document.createElement(current.firstElementChild.nodeName);
         newElement.innerHTML = "Новый элемент";
         current.appendChild(newElement);
         f2(current.parentElement);
+
     }
-    
     
 }
 
